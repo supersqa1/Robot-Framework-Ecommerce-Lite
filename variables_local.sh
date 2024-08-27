831012b32cdebf353b99dfa2c93db8b29a5ba55e
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2024-08-26 22:20:33 -0700

    modified readme and added command examples

---
+++ variables_local.sh
@@ -0,0 +1,16 @@
+
+
+export BROWSER=chrome
+export BASE_URL=http://dev.bootcamp.store.supersqa.com
+
+export RESULTS_DIR=$(pwd)/results
+export DB_PORT=<your database port>
+export DB_HOST=<your database host>
+export DB_DATABASE=<your site's database/schema name>
+export DB_TABLE_PREFIX=<your site's tables prefix>
+
+# credentials (these should not be kept in source controle like GitHub)
+export WOO_KEY=<your woocommerce api key>
+export WOO_SECRET=<your woocommerce api secret>
+export DB_USER=<your database user>
+export DB_PASSWORD=<your database password>
